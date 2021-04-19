fb4621d553267b3e58a91c1a7f2534467a476e53
jnoxro <jackorton32@gmail.com>
2021-04-19 16:42:12 +0100

    willy-am

--- project/main.c
+++ project/main.c
@@ -1,53 +1,74 @@
-#include "epuck1x/uart/e_uart_char.h"
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
-
-
 #include "leds.h"
-
+#include "epuck1x/uart/e_uart_char.h"
 #include "spi_comm.h"
 #include "serial_comm.h"
-
+#include "motors.h"
 
 #include "ch.h"
 #include "hal.h"
-#include "motors.h"
+
 #include "memory_protection.h"
 #include <main.h>
 
-void clear_leds(void);
-void spi_comm_start(void);
-void motors_init(void);
-void serial_start(void);
-void e_send_uart1_char(const char * buff, int buff_len);
+//void set_body_led(unsigned int value);
+//void clear_leds(void);
+//void spi_comm_start(void);
+//void motors_init(void);
+//void serial_start(void);
+//void e_send_uart1_char(const char * buff, int buff_len);
+
+
 
 int main(void)
 {
+   // serial_start();
 
+  //  clear_leds();
+   // spi_comm_start();
     halInit();
     chSysInit();
     mpu_init();
 
 
+    motors_init();
+    //chThdSleepMilliseconds(1000);
+
     /* Infinite loop. */
     while (1)
     {
-    	unsigned int on = 1;
-    	unsigned int off = 0;
-    	void set_body_led(on);
-    	chThdSleepMilliseconds(500);
+    	//unsigned int on = 1;
+    	//unsigned int off = 0;
+    	//set_body_led(on);
+    	left_motor_set_speed(500);
+    	right_motor_set_speed(-500);
+    	chThdSleepMilliseconds(1000);
 
 
-    	charstr[100];
-    	int str_length;
-    	str_length = sprintf(str, "Hello World\n");
-    	e_send_uart1_char(str, str_length);
+    	//char str[100];
+    	//int str_length;
+
+    	//str_length = sprintf(str, "Hello World\n");
+    	//e_send_uart1_char(str, str_length);
     	//void set_led(LED1,0);
     	//chThdSleepMilliseconds(500);
+
+
+    	//int value=10;
+    	//str_length = sprintf(str, "Printing number %d!\n",value);
+    	//e_send_uart1_char(str, str_length);
+
+    	//set_body_led(off);
+    	left_motor_set_speed(-500);
+    	right_motor_set_speed(500);
+    	chThdSleepMilliseconds(1000);
+
     }
 }
 
